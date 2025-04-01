863215af92b49ca58640c2bb394afd58c409ad8c
Suriya2023 <rajputsuraj221221@gmail.com>
2025-04-01 18:42:17 +0530

    add functionality to cart add button

--- my-react-app/src/componet/Cart.jsx
+++ my-react-app/src/componet/Cart.jsx
@@ -1,8 +1,37 @@
 import React from 'react'
 
-function cart() {
+function cart({ cart, SetCart }) {
     return (
         <div>
+
+            <div className="conatainer">
+                {
+                    cart.map((e) => {
+                        return (
+
+                            <div className="col-lg-5">
+                                {/* <div className="flex"> */}
+                                <div className="card mb-3 " style={{ width: "540px" }}>
+                                    <div className="row g-0">
+                                        <div className="col-lg-3">
+                                            <img src={e.imgSrc} className="img-fluid rounded-start" alt="Img" />
+                                        </div>
+                                        <div className="col-md-8">
+                                            <div className="card-body">
+                                                <h5 className="card-title">{e.title}</h5>
+                                                <p className="card-text">{e.description}</p>
+                                                <p className="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                {/* </div> */}
+                            </div>
+                        )
+                    })
+                }
+            </div>
+
 
         </div>
     )
--- my-react-app/src/componet/navbar.jsx
+++ my-react-app/src/componet/navbar.jsx
@@ -54,15 +54,15 @@ function navbar({ SetData, cart }) {
 
 
                             </form>
-                            <h5><Link to={"/"}>
+                            <h5><Link to={'/Cart'}>
 
-                                <button type="button" class="btn btn-primary position-relative">
+                                <Link to={'/Cart'} type="button" className="btn btn-primary position-relative">
                                     Inbox
-                                    <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+                                    <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
                                         {cart.length}
-                                        <span class="visually-hidden">unread messages</span>
+                                        <span className="visually-hidden">unread messages</span>
                                     </span>
-                                </button>
+                                </Link>
 
                             </Link></h5>
                         </div>
